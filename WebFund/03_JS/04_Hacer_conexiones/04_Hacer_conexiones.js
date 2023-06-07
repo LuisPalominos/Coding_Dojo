7547377aa160bb8b3ca6cbe7f6fc75c7d1d5f652
Luis Palominos <vicenteflaquito@gmail.com>
2023-06-06 20:30:54 -0400

    agragando JS a tareas anteriores

--- WebFund/03_JS/04_Hacer_conexiones/04_Hacer_conexiones.js
+++ WebFund/03_JS/04_Hacer_conexiones/04_Hacer_conexiones.js
@@ -1,21 +1,33 @@
 var cambioName = document.querySelector(".name");
 var cambioRequest = [document.querySelector("#numero1"),document.querySelector("#numero2")];
 var countRequest = [2,500];
-
+var x = document.querySelector(".persona1");
+var y = document.querySelector(".persona2");
 function cambioNombre(){
     cambioName.innerText = "Luisa Rodriguez";
 }
 
-function add() {
+function add1() {
     countRequest[0]--;
     countRequest[1]++;
     cambioRequest[0].innerText = countRequest[0];
     cambioRequest[1].innerText = countRequest[1];
+    x.remove();
 }
-function deny() {
+function deny1() {
     countRequest[0]--;
     cambioRequest[0].innerText = countRequest[0];
-}
-function borrar(x){
     x.remove();
+}
+function add2() {
+    countRequest[0]--;
+    countRequest[1]++;
+    cambioRequest[0].innerText = countRequest[0];
+    cambioRequest[1].innerText = countRequest[1];
+    y.remove();
+}
+function deny2() {
+    countRequest[0]--;
+    cambioRequest[0].innerText = countRequest[0];
+    y.remove();
 }
