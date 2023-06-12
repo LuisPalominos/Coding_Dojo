0a3b07a77b4dcd41a585bafad7df7d9777116e9b
Luis Palominos <vicenteflaquito@gmail.com>
2023-06-12 18:20:56 -0400

    Practicando bucles

---
+++ WebFund/03_JS/09_Desafios_bucles/09_Desafios_bucles.js
@@ -0,0 +1,37 @@
+// ------Imprimir Impares-------
+
+// for(var i=1;i<=20;i++){
+//     if(i%2==!0){
+//         console.log(i);
+//     }
+// }
+
+// ----------Disminuir multiplos de 3-------------
+
+// for(var i=100;i>=0;i--){
+//     if(i%3==0){
+//         console.log(i);
+//     }
+// }
+
+// ---------------Imprime la secuencia------------
+
+// for(var i=4;i>=-3.5;i-=1.5){
+//     console.log(i);
+// }
+
+// ---------------Sigma---------------------
+
+// var sum=0;
+// for(i=1;i<=100;i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+// ------------Factorial----------------------
+
+var product=1;
+for(i=1;i<=12;i++){
+    product*=i;
+}
+console.log(product);
