e34193706855a2eeed82b5df8056d27e22421c15
Luis Palominos <vicenteflaquito@gmail.com>
2023-06-23 21:55:47 -0400

    convertir a arrow function(inconclusa)

--- MERN/01_JS_Advance/03_Convertir_a_flecha/03_Convertir_a_flecha.js
+++ MERN/01_JS_Advance/03_Convertir_a_flecha/03_Convertir_a_flecha.js
@@ -1,25 +1,30 @@
-document.getElementById("button").onclick=() => {
-    setBackgroundColorById("paragraph", "blue");
-}
-document.getElementById("alert").onclick = () => {
-    alert(document.getElementById("popup-input").value);
-}
-document.getElementById("hover-this").onmouseover = () => {
-    setBackgroundColorById("body", "red");
-}
-document.getElementById("hover-this").onmouseout = () => {
-    setBackgroundColorById("body", "white");
-}
 function getValueFromId(id){
     return document.getElementById(id).value;
 }
 function setBackgroundColorById(id, color){
     document.getElementById(id).style = "background-color: " + color;
 }
-document.getElementById("task").onclick = () =>{
-    mouseOverFunction();
-}
-function mouseOverFunction(el){
-    let x= document.getElementById("color").value
-    el.style= "background-color: "+ x;
-}
+
+$(document).ready(()=> {
+    $("#button").on("click", () => {setBackgroundColorById("paragraph", "blue")});
+
+    document.getElementById("alert").onclick = () => {
+        alert(document.getElementById("popup-input").value);
+    }
+    document.getElementById("hover-this").onmouseover = () => {
+        setBackgroundColorById("body", "red");
+    }
+    document.getElementById("hover-this").onmouseout = () => {
+        setBackgroundColorById("body", "white");
+    }
+
+    // document.getElementById("task").onclick = () =>{
+    //     mouseOverFunction();
+    // }
+    // function mouseOverFunction(el){
+    //     let x= document.getElementById("color").value
+    //     el.style= "background-color: "+ x;
+    // }
+});
+
+
