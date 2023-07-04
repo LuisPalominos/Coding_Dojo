9846bdf71edae50ef3f2bc7ba157eb210a8fcd8a
Luis Palominos <vicenteflaquito@gmail.com>
2023-07-04 16:09:42 -0400

    Primer React con JSX

---
+++ MERN/02_React/02_primer_react_jsx/src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container text-center mt-5">
+        <h1>Hello Dojo</h1>
+        <h2>Things i need to do:</h2>
+        <ul class="list-group">
+          <li class="list-group-item">Leran React</li>
+          <li class="list-group-item">Climb Mt. Everest</li>
+          <li class="list-group-item">Run  marathon</li>
+          <li class="list-group-item">Feed the dogs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
