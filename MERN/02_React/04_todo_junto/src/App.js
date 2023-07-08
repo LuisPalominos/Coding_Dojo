c704bd4c35754f6c7981f74c3afa1bb68ff3d52e
Luis Palominos <vicenteflaquito@gmail.com>
2023-07-07 20:45:24 -0400

    ejercicio de estado (inconcluso)

--- MERN/02_React/04_todo_junto/src/App.js
+++ MERN/02_React/04_todo_junto/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Cards from "./components/Cards";
-import Button from "./components/Button";
 const App=()=> {
   return (
     <div className="App">
@@ -10,31 +9,24 @@ const App=()=> {
         age={45}
         hairColor="Black"
       />
-      <Button/>
-      <hr className="w-25 mx-auto"/>
       <Cards
         FirstName="John"
         LastName="Smith"
         age={88}
         hairColor="Brown"
       />
-      <Button/>
-      <hr className="w-25 mx-auto"/>
       <Cards
         FirstName="Millard"
         LastName="Fillmore"
         age={50}
         hairColor="Brown"
       />
-      <Button/>
-      <hr className="w-25 mx-auto"/>
       <Cards
         FirstName="Maria"
         LastName="Smith"
         age={62}
         hairColor="Brown"
       /> 
-      <Button/>
     </div>
   );
 }
--- MERN/02_React/04_todo_junto/src/components/Cards.js
+++ MERN/02_React/04_todo_junto/src/components/Cards.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Cards = (props) => {
 // ---------------------------------------------
 // I) VARIABLES & HOOKS
 // ---------------------------------------------
+let [Count, setCount] = useState(props.age);
 
 
 // ---------------------------------------------
 // II) HANDLERS & AUX FUNCTIONS
 // ---------------------------------------------
+const handleOnClick = () =>{
+    console.log(Count);
+    let newcount=Count +1;
+    setCount(newcount);
+    console.log(newcount);
+    let edad=document.querySelector("#age");
+    edad.innerText="Age: " + newcount;
+    }
 
 
 // ---------------------------------------------
@@ -17,9 +26,11 @@ const Cards = (props) => {
 
     return (
         <div className="container d-flex flex-column align-items-center">
-                <h1 className='col-3 ms-5 ps-5 fs-2 fw-bold '>{props.LastName}, {props.FirstName}</h1>
-                <p className='col-3 ms-5 ps-5'>Age: {props.age}</p>
-                <p className='col-3 ms-5 ps-5'>Hair Color: {props.hairColor}</p>
+            <h1 className='col-3 ms-5 ps-5 fs-2 fw-bold '>{props.LastName}, {props.FirstName}</h1>
+            <p id='age' className='col-3 ms-5 ps-5'>Age: {props.age}</p>
+            <p className='col-3 ms-5 ps-5'>Hair Color: {props.hairColor}</p>
+            <button  type="button" className="btn btn-primary" onClick={handleOnClick}>Birthday Button of {props.FirstName} {props.LastName}</button>
+            <hr className="w-50 mx-auto"/>
         </div>
     )
 }
