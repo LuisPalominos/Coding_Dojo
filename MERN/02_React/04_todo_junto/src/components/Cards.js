cb6610606a3aeb51f5916f6ad8d55e827eb54c7f
Luis Palominos <vicenteflaquito@gmail.com>
2023-07-09 23:28:26 -0400

    ejercicio de estado

--- MERN/02_React/04_todo_junto/src/components/Cards.js
+++ MERN/02_React/04_todo_junto/src/components/Cards.js
@@ -11,15 +11,9 @@ let [Count, setCount] = useState(props.age);
 // II) HANDLERS & AUX FUNCTIONS
 // ---------------------------------------------
 const handleOnClick = () =>{
-    console.log(Count);
     let newcount=Count +1;
-    setCount(newcount);
-    console.log(newcount);
-    
-    let edad=document.querySelector("#age");
-    edad.innerText="Age: " + newcount;
+    setCount(newcount); 
     }
-
 
 // ---------------------------------------------
 // III) JSX
@@ -28,7 +22,7 @@ const handleOnClick = () =>{
     return (
         <div className="container d-flex flex-column align-items-center">
             <h1 className='col-3 ms-5 ps-5 fs-2 fw-bold '>{props.LastName}, {props.FirstName}</h1>
-            <p id='age' className='col-3 ms-5 ps-5'>Age: {props.age}</p>
+            <p id='age' className='col-3 ms-5 ps-5'>Age: {Count}</p>
             <p className='col-3 ms-5 ps-5'>Hair Color: {props.hairColor}</p>
             <button  type="button" className="btn btn-primary" onClick={handleOnClick}>Birthday Button of {props.FirstName} {props.LastName}</button>
             <hr className="w-50 mx-auto"/>
