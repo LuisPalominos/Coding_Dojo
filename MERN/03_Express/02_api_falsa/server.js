fb08a60e79deb28f0ba1c167f8049880189871c7
Luis Palominos <vicenteflaquito@gmail.com>
2023-07-19 17:08:33 -0400

    API Faker

---
+++ MERN/03_Express/02_api_falsa/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { faker } = require('@faker-js/faker');
+const app = express();
+const port = 8000;
+
+class Usuario {
+    constructor() {
+        this.userId= faker.string.uuid(),
+        this.firstname= faker.person.firstName(),
+        this.lastname= faker.person.lastName(),
+        this.phonenumber= faker.phone.number(),
+        this.email= faker.internet.email(),
+        this.password= faker.internet.password()
+    }
+}
+class Empresa {
+    constructor() {
+        this.userId= faker.string.uuid(),
+        this.companyname= faker.company.name(),
+        this.direction={
+            street: faker.location.street(),
+            city: faker.location.city(),
+            state: faker.location.state(),
+            zipcode: faker.location.zipCode(),
+            country: faker.location.country()
+        }
+    }
+}
+
+
+app.get("/api/users/new", (req, res) => {
+    res.json({ message: new Usuario() });
+});
+app.get("/api/companies/new", (req, res) => {
+    res.json({ message: new Empresa() });
+});
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
+
+
